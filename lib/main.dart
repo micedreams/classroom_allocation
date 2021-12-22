98e65df82f8da4c01fb1d6e47439d9360d61af03
aku <micedreams@gmail.com>
2021-12-22 13:43:27 +0530

    misc

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:classroom_allocation/rest.dart';
+import 'package:classroom_allocation/my_home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,58 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  Future<void> _incrementCounter() async {
-    //var a = await Rest.get("students");
-    //print(a);
-    //var b = await Rest.post("registration", {"student": "1", "subject": "3"});
-    //print(b);
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: const MyHomePage(title: ''),
     );
   }
 }
